f5d368d0bae31b2c634a83f94da2991c46bd3fcc
Mirko Rapisarda <m.rapisarda@acadevmy.it>
2022-12-26 10:58:55 +0100

    feat: export extension methods

--- lib/dart_validator.dart
+++ lib/dart_validator.dart
@@ -1,1 +1,6 @@
 library dart_validator;
+
+export 'src/extension_methods/iterable_validators.dart';
+export 'src/extension_methods/map_validators.dart';
+export 'src/extension_methods/number_validators.dart';
+export 'src/extension_methods/string_validators.dart';
